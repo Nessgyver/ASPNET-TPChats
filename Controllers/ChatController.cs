3ff54378b94609bbdb4fc0d2f417357c9db75d2f
Damien ROUXEL <damien.rouxel2020@campus-eni.fr>
2020-09-16 15:34:59 +0200

    first commit

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP.Models;
+using TPModule3.Database;
+
+namespace TPChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        List<Chat> meuteDeChats = FakeDb.Instance.GetMeuteDeChats();
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(meuteDeChats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            Chat chatDetail = meuteDeChats.Where(c=>c.Id==id).FirstOrDefault();
+            return View(chatDetail);
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Chat chatSupprimer = meuteDeChats.Where(c => c.Id == id).FirstOrDefault();
+            return View(chatSupprimer);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                Chat chatSupprimer = meuteDeChats.Where(c => c.Id == id).FirstOrDefault();
+                meuteDeChats.Remove(chatSupprimer);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Database/FakeDB.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP.Models;
+
+namespace TPModule3.Database
+{
+    public class FakeDb
+    {
+        private static readonly Lazy<FakeDb> lazy =
+            new Lazy<FakeDb>(() => new FakeDb());
+
+        /// <summary>
+        /// FakeDb singleton access.
+        /// </summary>
+        public static FakeDb Instance { get { return lazy.Value; } }
+
+        private FakeDb()
+        {
+            this.ListeChats = InitialiserDatas();
+        }
+
+        public List<Chat> ListeChats { get; set; }
+
+        private List<Chat> InitialiserDatas()
+        {
+            var i = 1;
+            return new List<Chat>
+            {
+                new Chat{Id=i++,Nom = "Felix",Age = 3,Couleur = "Roux"},
+                new Chat{Id=i++,Nom = "Minette",Age = 1,Couleur = "Noire"},
+                new Chat{Id=i++,Nom = "Miss",Age = 10,Couleur = "Blanche"},
+                new Chat{Id=i++,Nom = "Garfield",Age = 6,Couleur = "Gris"},
+                new Chat{Id=i++,Nom = "Chatran",Age = 4,Couleur = "Fauve"},
+                new Chat{Id=i++,Nom = "Minou",Age = 2,Couleur = "Blanc"},
+                new Chat{Id=i,Nom = "Bichette",Age = 12,Couleur = "Rousse"}
+            };
+        }
+
+        public List<Chat> GetMeuteDeChats() {
+            return this.ListeChats;
+        }
+    }
+}
